53467ffd7148380bca516751c07b2a2f34f957ea
Daniel Nauck <d.nauck@nauck-it.de>
2013-01-30 22:18:24 +0100

    Automatically assign a new GUID when creating a license

---
+++ src/License.Manager/Common/UserCode/License.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class License
+    {
+        partial void License_Created()
+        {
+            if (LicenseId == null || LicenseId == Guid.Empty)
+                LicenseId = Guid.NewGuid();
+        }
+    }
+}
