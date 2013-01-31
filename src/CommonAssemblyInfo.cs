98e8f9a88d174210eb1d93946c56bf5e42c47b14
Daniel Nauck <d.nauck@nauck-it.de>
2013-01-31 16:39:10 +0100

    Add common assembly info

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompanyAttribute("Nauck IT KG")]
+[assembly: AssemblyProductAttribute("License Manager Light")]
+[assembly: AssemblyCopyrightAttribute("Copyright � 2012 - 2013 Nauck IT KG")]
+[assembly: AssemblyVersionAttribute("0.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("0.0.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.0.0-devel")]
