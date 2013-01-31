7ed1f82971dbb3b27b19929e45f0d062e0effeb0
Daniel Nauck <d.nauck@nauck-it.de>
2013-01-31 16:39:47 +0100

    Add FAKE build script

---
+++ build.fsx
@@ -0,0 +1,62 @@
+// include Fake lib
+#r @"Tools/FAKE/FakeLib.dll"
+open Fake
+open Fake.AssemblyInfoFile
+open System
+
+// build properties
+let buildTarget = getBuildParamOrDefault "buildTarget" "Publish"
+let buildConfiguration = getBuildParamOrDefault "buildConfiguration" "Release"
+
+// common path properties
+let sourceDir = currentDirectory @@ "src"
+let publishDir = currentDirectory @@ "Publish"
+let solutionFile = sourceDir @@ "License.Manager.sln"
+
+// tools path properties
+let toolsDir = currentDirectory @@ "Tools"
+
+// common assembly info properties
+let assemblyVersion = getBuildParamOrDefault "assemblyVersion" "0.0.0.0"
+let assemblyFileVersion = getBuildParamOrDefault "assemblyFileVersion" "0.0.0.0"
+let assemblyInformationalVersion = getBuildParamOrDefault "assemblyInformationalVersion" "0.0.0-devel"
+
+// Targets
+Target "All" DoNothing
+
+Target "Clean" (fun _ ->
+    let directoriesToClean =
+        !+ publishDir |> Scan
+
+    CleanDirs directoriesToClean
+)
+
+Target "CreateAssemblyInfo" (fun _ ->
+    CreateCSharpAssemblyInfo (sourceDir @@ "CommonAssemblyInfo.cs")
+        [
+        Attribute.Company "Nauck IT KG"
+        Attribute.Product "License Manager Light"
+        Attribute.Copyright (sprintf "Copyright © 2012 - %A Nauck IT KG" DateTime.Now.Year)
+        Attribute.Version assemblyVersion
+        Attribute.FileVersion assemblyFileVersion
+        Attribute.InformationalVersion assemblyInformationalVersion
+        ]
+)
+
+Target "Build" (fun _ ->
+    build (fun msbuild ->
+        {msbuild with
+            Targets = [buildTarget]
+            Properties = ["Configuration", buildConfiguration]
+        }
+    ) solutionFile
+)
+
+// Dependencies
+"Clean" 
+    ==> "CreateAssemblyInfo"
+    ==> "Build"
+    ==> "All"
+ 
+// start build
+Run <| getBuildParamOrDefault "target" "All"
