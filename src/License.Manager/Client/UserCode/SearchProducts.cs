fcac95ba224042377efe7621cb60ed3e35e4abf0
Daniel Nauck <d.nauck@nauck-it.de>
2013-02-01 22:08:22 +0100

    Add "New Project" button at the products list screen

---
+++ src/License.Manager/Client/UserCode/SearchProducts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+namespace LightSwitchApplication
+{
+    public partial class SearchProducts
+    {
+        partial void CreateProduct_Execute()
+        {
+            Application.ShowCreateNewProduct();
+        }
+    }
+}
