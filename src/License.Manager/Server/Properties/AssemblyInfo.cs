8d3e2128e88b616b4a7586ba27b665bc34dbc2fe
Daniel Nauck <d.nauck@nauck-it.de>
2013-02-05 21:13:36 +0100

    Revert "use new common assembly info file"
    This reverts commit 2311fd302f0b615ec14bf3d19f3e93075c47a700.

--- src/License.Manager/Server/Properties/AssemblyInfo.cs
+++ src/License.Manager/Server/Properties/AssemblyInfo.cs
@@ -16,3 +16,15 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7f992408-34d5-4628-8455-948f9a0f9c69")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
